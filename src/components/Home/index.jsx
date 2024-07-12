21b9d688916b7a152a861c7250646f0a3ca15838
Matthew Idungafa <mattidungafa@gmail.com>
2024-07-12 09:00:48 +0100

    started promotion component

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 //import "./index.scss";
 import Hero from "./hero";
+import Promotion from "./promotion";
 
 
 const Home = () => {
---
+++ src/components/Home/promotion/index.jsx
@@ -0,0 +1,30 @@
+import './index.scss'
+const Promotion = () => {
+  return (
+      <div className="promotion">
+         <div className="promotion__container"> 
+            <div className="promotion-items">
+                <div className="promotion-item">
+                <p className="promotion-item__icon">  </p>
+                <p className="promotion-item__title"> Free shipping </p>
+                <p className="promotion-item__description"> On all orders over $75 </p>
+                </div>
+                <div className="promotion-item">
+                <p className="promotion-item__icon">  </p>
+                <p className="promotion-item__title"> Free shipping </p>
+                <p className="promotion-item__description"> On all orders over $75 </p>
+                </div>
+                <div className="promotion-item">
+                <p className="promotion-item__icon">  </p>
+                <p className="promotion-item__title"> Free shipping </p>
+                <p className="promotion-item__description"> On all orders over $75 </p>
+            </div>
+          </div>
+        </div>
+
+        
+      </div>
+  );
+}
+
+export default Promotion;
